4a7765c19b91397f186cbb74990c39b1e5f709a8
EstebanRuizz <rubianruiz05@my.com>
2024-04-06 09:40:39 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutComponent } from './presentation/about/about.component';
+import { CodeComponent } from './presentation/code/code.component';
+import { ContactComponent } from './presentation/contact/contact.component';
+import { CoursesComponent } from './presentation/courses/courses.component';
+import { ExperienceComponent } from './presentation/experience/experience.component';
+import { HomeComponent } from './presentation/home/home.component';
+import { ProjectsComponent } from './presentation/projects/projects.component';
+import { StackComponent } from './presentation/stack/stack.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
+  },
+  {
+    path: 'stack',
+    component: StackComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'code',
+    component: CodeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
