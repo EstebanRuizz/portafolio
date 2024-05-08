f036b2a30966bf921b7f5f941ffc8735838bd90b
EstebanRuizz <rubianruiz05@my.com>
2024-05-08 16:32:08 -0500

    Feature; sidebar

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -1,18 +1,19 @@
-import { SplitInterpolation } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [SidebarComponent]
 })
 export class HomeComponent {
   constructor(private router: Router) {}
 
-  public readonly imgUrl: string = `https://media.licdn.com/dms/image/D4E03AQGxYVWihQpbxg/profile-displayphoto-shrink_400_400/0/1708996732257?e=1717632000&v=beta&t=7CACUCiF5dI6SKn8ZYxaGPzbtevKXLTptQbNy31C_Y0`;
+  public readonly imgUrl: string = `https://th.bing.com/th/id/R.0e4b3d762aa98b49d712f4e5e394ba6c?rik=sFQZKke1MannAw&pid=ImgRaw&r=0`;
+  // public readonly imgUrl: string = `https://media.licdn.com/dms/image/D4E03AQGxYVWihQpbxg/profile-displayphoto-shrink_400_400/0/1708996732257?e=1717632000&v=beta&t=7CACUCiF5dI6SKn8ZYxaGPzbtevKXLTptQbNy31C_Y0`;
 
   public navigateTo(route: string): void {
     this.router.navigate([route]);
